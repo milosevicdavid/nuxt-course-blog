811e66484d80b2c281053add3e43aed0ff67d305
David <milosevicdavid@gmail.com>
2020-10-25 08:01:05 +0100

    Course

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Amet quia quam mollitia ex nihil! Cupiditate explicabo quam reiciendis hic eum.</p>
+    <p>Amet quia quam mollitia ex nihil! Cupiditate explicabo quam reiciendis hic eum.</p>
+    <p>Amet quia quam mollitia ex nihil! Cupiditate explicabo quam reiciendis hic eum.</p>
+    <p>Amet quia quam mollitia ex nihil! Cupiditate explicabo quam reiciendis hic eum.</p>
+    <p>Amet quia quam mollitia ex nihil! Cupiditate explicabo quam reiciendis hic eum.</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
